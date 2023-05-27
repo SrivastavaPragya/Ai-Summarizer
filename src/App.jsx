0974618cf0824acf2117c455fef6d1d9115343ad
Pragya <pragya142018@gmail.com>
2023-05-28 00:28:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from "./components/Hero"
+import Demo from './components/Demo'
+import "./App.css"
+
+const App = () => {
+  return (
+    <main>
+      <div className='main'>
+        <div className='gradient' />
+      </div>
+
+      <div className='app'>
+      
+        <Demo />
+        <Hero />
+      </div>
+    </main>
+  );
+};
+
+export default App;
